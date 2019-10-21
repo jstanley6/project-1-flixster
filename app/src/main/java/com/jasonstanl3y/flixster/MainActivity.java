0f84fa1becf889afeef9bf2d1439f4340128b952
Jason Stanley <jasonstanl3y@gmail.com>
2019-10-20 17:41:08 -0700

    created you tube player view to play trailers of movies now playing. Created a detail activity for a user to view more details including rating of movies

--- app/src/main/java/com/jasonstanl3y/flixster/MainActivity.java
+++ app/src/main/java/com/jasonstanl3y/flixster/MainActivity.java
@@ -23,6 +23,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String CONFIGURATIONS_API = "https://api.themoviedb.org/3/configuration?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     public static final String TAG = "MainActivity";
     List<Movie> movies;
     @Override
@@ -46,6 +47,27 @@ public class MainActivity extends AppCompatActivity {
 
         //set a layout manager on the recycler view
         rvMovies.setLayoutManager(new LinearLayoutManager(this));
+
+        AsyncHttpClient images_client = new AsyncHttpClient();
+        images_client.get(CONFIGURATIONS_API, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    jsonObject = jsonObject.getJSONObject("images");
+                    JSONArray images = jsonObject.getJSONArray("poster_sizes");
+                   // Log.i(TAG, "poster_sizes " + images.get(2));
+                } catch (JSONException e) {
+                  //  Log.e(TAG, "JSON Exception hit", e);
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+              //  Log.d(TAG, "onFailure");
+            }
+        });
 
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
--- app/src/main/java/com/jasonstanl3y/flixster/models/Movie.java
+++ app/src/main/java/com/jasonstanl3y/flixster/models/Movie.java
@@ -1,5 +1,10 @@
 package com.jasonstanl3y.flixster.models;
 
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -7,6 +12,8 @@ import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Headers;
 
 @Parcel
 public class Movie {
@@ -16,7 +23,9 @@ public class Movie {
     String overView;
     String backDropPath;
     double rating;
+    String posterSize;
 
+    List<Movie> imageSizes;
 
     // empty constructor needed by the Parceler library
     public Movie() {}
@@ -45,10 +54,12 @@ public class Movie {
 
 
     public String getPosterPath() {
+
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
     }
 
     public String getBackDropPath() {
+
         return String.format("https://image.tmdb.org/t/p/w342/%s", backDropPath);
     }
 
